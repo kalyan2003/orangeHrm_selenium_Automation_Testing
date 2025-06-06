ea62bb45b38dda0ae7f56c63fccc70887e470af6
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-06-06 22:34:57 +0530

    Updated the bat file

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 call C:\Users\pavan\Documents\Selenium_Projects\nopCommerce_Project\.venv\Scripts\activate.bat
-pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
